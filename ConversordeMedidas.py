f07f7b0baa4625e0933d5d7a8cb483b6d1e1ce4a
Pedro Ewen <116721969+PedroEwen@users.noreply.github.com>
2022-12-31 15:00:21 -0300

    Create ConversordeMedidas.py

---
+++ ConversordeMedidas.py
@@ -0,0 +1,10 @@
+print('-------------------------')
+print('  CONVERSOR DE MEDIDAS   ')
+print('-------------------------')
+m = int(input('Valor para a conversão em metros:'))
+dm = m*10
+cm = m*100
+mm = m*1000
+print(m ,'metros equivale a:', dm, 'dm')
+print(m ,'metros equivale a:', cm, 'cm')
+print(m, 'metros equivale a:', mm, 'mm')
