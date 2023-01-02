4c17d1e5c83de50b6aedf42c689f70d97ffd45bf
Pedro Ewen <116721969+PedroEwen@users.noreply.github.com>
2023-01-02 15:21:03 -0300

    Create Locadora.py

---
+++ Locadora.py
@@ -0,0 +1,9 @@
+print('         SilvaCar Locadora           ')
+print('    OS MELHORES PREÇOS ESTÃO AQUI!   ')
+print('Alugue carros com a melhor do mercado!')
+print('-----------------------------------------')
+print('Simulação de alocação')
+dias = int(input('Quantas dias deseja alocar:'))
+valor = 100 #valor por dias
+v = valor * dias
+print('Você deverá pagar:', v, 'reais')
