228c814d3915d9a4957b46e7dffeec6412c6f96d
Pedro Ewen <116721969+PedroEwen@users.noreply.github.com>
2023-01-02 20:54:26 -0300

    Create Trigonometria.py

---
+++ Trigonometria.py
@@ -0,0 +1,8 @@
+import math
+ang = int(input('Digite o ãngulo desejado:'))
+sen = math.sin(math.radians(ang))
+cos = math.cos(math.radians(ang))
+tan = math.tan(math.radians(ang))
+print('Seno de', ang,'é igual a:', sen)
+print('Cosseno de', ang, 'é igual a:', cos)
+print('Tangente de', ang, 'é igual a:', tan,)
