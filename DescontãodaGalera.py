bd07738c043d856cca311f7913c4b87276530bcd
Pedro Ewen <116721969+PedroEwen@users.noreply.github.com>
2023-01-02 21:45:11 -0300

    Create DescontãodaGalera.py

---
+++ DescontãodaGalera.py
@@ -0,0 +1,8 @@
+print('[-------------------]')
+print('|Descontão da galera|')
+print('[-------------------]')
+valori = float(input('Valor original:'))
+desc = float(input('Desconto:'))
+calc = (desc/100) * valori
+valf = calc + valori
+print('O valor a pagar será de', valf)
